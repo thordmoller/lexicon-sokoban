9ef8f290cc07f7a7b57adb043e3307f2fdc8ed3e
Thord Möller <thord_moller_92@hotmail.com>
2023-12-15 06:39:03 +0100

    added eventlistener for keypress with movable player not able to walk into walls

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const gameContainer = document.getElementById("game-container");
 
+let map = generateMap(tileMap01);
+
 
 
 function generateMap(tileMap) {
@@ -21,18 +23,87 @@ function generateMap(tileMap) {
             const tileElement = document.createElement("div");
             tileElement.id = `x${col}y${row}`;
 
+            tileElement.setAttribute("data-x", col.toString());
+            tileElement.setAttribute("data-y", row.toString());
+
+            tileElement.classList.add("tile");
+
             if (tileType != " ") {
-                tileElement.classList.add("tile", `tile-${tileType}`);
+                tileElement.classList.add(`tile-${tileType}`);
             }
 
             gameContainer.appendChild(tileElement);
         }
     }
+
+    return tileMap;
 }
 
 function setGridDimensions(tileMap){
-    gameContainer.style.gridTemplateColumns = `repeat(${tileMap.width}, 2.5vw`;
-    gameContainer.style.gridTemplateRows = `repeat(${tileMap.height}, 2.5vw`;
-};
+    gameContainer.style.gridTemplateColumns = `repeat(${tileMap.width}, 2.5vw)`;
+    gameContainer.style.gridTemplateRows = `repeat(${tileMap.height}, 2.5vw)`;
+}
 
-generateMap(tileMap01);
+
+// Function to move the player
+function movePlayer(direction) {
+
+    const initialTile = getCurrentPlayerPosition();
+    if (initialTile) {
+
+        var movedPos = {
+            x: parseInt(initialTile.dataset.x, 10),
+            y: parseInt(initialTile.dataset.y, 10)
+        }
+
+        switch(direction){
+            case 'up':
+            movedPos.y -= 1;
+            break;
+            case 'down':
+                movedPos.y += 1;
+                break;
+            case 'left':
+                movedPos.x -= 1;
+                break;
+            case 'right':
+                movedPos.x += 1;
+                break;
+        }
+        console.log(movedPos)
+        movedTileType = map.mapGrid[movedPos.y][movedPos.x][0];
+        console.log(movedTileType);
+
+        if(movedTileType === " " || movedTileType === "P" || movedTileType === "G"){
+            initialTile.classList.remove('tile-P');
+            const id = 'x' + movedPos.x + 'y' + movedPos.y;
+            let movedTile = document.getElementById(id);
+            movedTile.classList.add('tile-P');
+        }
+    }
+}
+
+function getCurrentPlayerPosition(){
+    // Set initial player position
+    const initialPlayerElement = document.querySelector('.tile-P');
+    
+    return initialPlayerElement;
+}
+
+document.addEventListener('keydown', function(event) {
+    switch (event.code) {
+        case 'ArrowUp':
+            movePlayer('up');
+            console.log('up');
+            break;
+        case 'ArrowDown':
+            movePlayer('down');
+            break;
+        case 'ArrowLeft':
+            movePlayer('left');
+            break;
+        case 'ArrowRight':
+            movePlayer('right');
+            break;
+    }
+});
