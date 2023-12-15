fc6ee29c9dc1bc3b5101c13ad979b51ba62c1769
Thord Möller <thord_moller_92@hotmail.com>
2023-12-15 09:34:15 +0100

    made boxes movable

--- game.js
+++ game.js
@@ -2,108 +2,158 @@ const gameContainer = document.getElementById("game-container");
 
 let map = generateMap(tileMap01);
 
-
-
 function generateMap(tileMap) {
-    // Ensure that the game container exists
-    if (!gameContainer) {
-        console.error("Game container not found!");
-        return;
-    }
+	// Ensure that the game container exists
+	if (!gameContainer) {
+		console.error("Game container not found!");
+		return;
+	}
 
-    setGridDimensions(tileMap);
+	setGridDimensions(tileMap);
 
+	for (let row = 0; row < tileMap.height; row++) {
+		for (let col = 0; col < tileMap.width; col++) {
+			let tileType = tileMap.mapGrid[row][col][0];
 
-    for (let row = 0; row < tileMap.height; row++) {
-        for (let col = 0; col < tileMap.width; col++) {
+			const tileElement = document.createElement("div");
+			tileElement.id = `x${col}y${row}`;
 
+			tileElement.setAttribute("data-x", col.toString());
+			tileElement.setAttribute("data-y", row.toString());
 
-            let tileType = tileMap.mapGrid[row][col][0];
+			tileElement.classList.add("tile");
 
-            const tileElement = document.createElement("div");
-            tileElement.id = `x${col}y${row}`;
+			if (tileType != " ") {
+				tileElement.classList.add(`tile-${tileType}`);
+			}
 
-            tileElement.setAttribute("data-x", col.toString());
-            tileElement.setAttribute("data-y", row.toString());
+			gameContainer.appendChild(tileElement);
+		}
+	}
 
-            tileElement.classList.add("tile");
-
-            if (tileType != " ") {
-                tileElement.classList.add(`tile-${tileType}`);
-            }
-
-            gameContainer.appendChild(tileElement);
-        }
-    }
-
-    return tileMap;
+	return tileMap;
 }
 
-function setGridDimensions(tileMap){
-    gameContainer.style.gridTemplateColumns = `repeat(${tileMap.width}, 2.5vw)`;
-    gameContainer.style.gridTemplateRows = `repeat(${tileMap.height}, 2.5vw)`;
+function setGridDimensions(tileMap) {
+	gameContainer.style.gridTemplateColumns = `repeat(${tileMap.width}, 2vw)`;
+	gameContainer.style.gridTemplateRows = `repeat(${tileMap.height}, 2vw)`;
 }
-
 
 // Function to move the player
-function movePlayer(direction) {
+function move(direction) {
+	const initialTile = getPlayerTile();
+	if (initialTile) {
+		var movedTile = getNewPosition(direction, getPlayerTile());
+		movedTileType =
+			map.mapGrid[movedTile.dataset.y][movedTile.dataset.x][0]; //checking the type of tile the player tries to move to
 
-    const initialTile = getCurrentPlayerPosition();
-    if (initialTile) {
-
-        var movedPos = {
-            x: parseInt(initialTile.dataset.x, 10),
-            y: parseInt(initialTile.dataset.y, 10)
-        }
-
-        switch(direction){
-            case 'up':
-            movedPos.y -= 1;
-            break;
-            case 'down':
-                movedPos.y += 1;
-                break;
-            case 'left':
-                movedPos.x -= 1;
-                break;
-            case 'right':
-                movedPos.x += 1;
-                break;
-        }
-        console.log(movedPos)
-        movedTileType = map.mapGrid[movedPos.y][movedPos.x][0];
-        console.log(movedTileType);
-
-        if(movedTileType === " " || movedTileType === "P" || movedTileType === "G"){
-            initialTile.classList.remove('tile-P');
-            const id = 'x' + movedPos.x + 'y' + movedPos.y;
-            let movedTile = document.getElementById(id);
-            movedTile.classList.add('tile-P');
-        }
-    }
+		//only actually moves if its not a wall
+		if (movedTile.classList.contains("tile-B")) {
+			if (moveBox(getNewBoxPosition(direction))) {
+				movePlayer(movedTile);
+			}
+		} else if (!movedTile.classList.contains("tile-W")) {
+			movePlayer(movedTile);
+		}
+	}
 }
 
-function getCurrentPlayerPosition(){
-    // Set initial player position
-    const initialPlayerElement = document.querySelector('.tile-P');
-    
-    return initialPlayerElement;
+function getNewPosition(direction, initialTile) {
+	//initial position
+	var movedPos = {
+		x: parseInt(initialTile.dataset.x, 10),
+		y: parseInt(initialTile.dataset.y, 10),
+	};
+
+	//changing position based on keypress
+	switch (direction) {
+		case "up":
+			movedPos.y -= 1;
+			break;
+		case "down":
+			movedPos.y += 1;
+			break;
+		case "left":
+			movedPos.x -= 1;
+			break;
+		case "right":
+			movedPos.x += 1;
+			break;
+	}
+
+	const id = "x" + movedPos.x + "y" + movedPos.y;
+
+	let movedTile = document.getElementById(id);
+
+	return movedTile;
 }
 
-document.addEventListener('keydown', function(event) {
-    switch (event.code) {
-        case 'ArrowUp':
-            movePlayer('up');
-            console.log('up');
-            break;
-        case 'ArrowDown':
-            movePlayer('down');
-            break;
-        case 'ArrowLeft':
-            movePlayer('left');
-            break;
-        case 'ArrowRight':
-            movePlayer('right');
-            break;
-    }
+function getNewBoxPosition(direction) {
+	initialTile = getPlayerTile();
+	initialBoxTile = getNewPosition(direction, initialTile);
+	movedBoxTile = getNewPosition(direction, initialBoxTile);
+
+	console.log(movedBoxTile.id);
+	var boxTiles = {
+		movedBoxTile,
+		initialBoxTile,
+	};
+	return boxTiles;
+}
+
+function movePlayer(movedTile) {
+	initialTile = getPlayerTile();
+	initialTile.classList.remove("tile-P");
+	movedTile.classList.add("tile-P");
+}
+function moveBox(boxTiles) {
+	const movedBoxTile = boxTiles.movedBoxTile;
+	const initialBoxTile = boxTiles.initialBoxTile;
+
+	console.log(movedBoxTile.classList);
+
+	if (
+		!["tile-W", "tile-B"].some((cls) =>
+			movedBoxTile.classList.contains(cls)
+		)
+	) {
+		initialBoxTile.classList.remove("tile-B");
+		movedBoxTile.classList.add("tile-B");
+		return true;
+	}
+	return false;
+}
+
+function getPlayerTile() {
+	playerTile = document.querySelector(".tile-P");
+
+	return playerTile;
+}
+
+document.addEventListener("keydown", function (event) {
+	// Prevent the default action for arrow keys
+	switch (event.code) {
+		case "ArrowUp":
+		case "ArrowDown":
+		case "ArrowLeft":
+		case "ArrowRight":
+			event.preventDefault();
+			break;
+	}
+
+	switch (event.code) {
+		case "ArrowUp":
+			move("up");
+			console.log("up");
+			break;
+		case "ArrowDown":
+			move("down");
+			break;
+		case "ArrowLeft":
+			move("left");
+			break;
+		case "ArrowRight":
+			move("right");
+			break;
+	}
 });
